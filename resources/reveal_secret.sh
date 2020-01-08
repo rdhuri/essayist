d3102c9827e00763c0af40fc19c4be75495d61ed
Rahul Dhuri <rdhuri@equalexperts.com>
2020-01-08 20:57:51 +0530

    update reveal_secret.sh

--- resources/reveal_secret.sh
+++ resources/reveal_secret.sh
@@ -3,6 +3,7 @@
 PRIVATE_KEY=$1
 GIT_SECRET_PASSPHRASE=$2
 
+sudo chmod +x ./gradlew
 sudo sh -c "echo 'deb https://dl.bintray.com/sobolevn/deb git-secret main' | tee -a /etc/apt/sources.list"
 sudo wget -qO - https://api.bintray.com/users/sobolevn/keys/gpg/public.key | sudo apt-key add -
 sudo apt-get update && sudo apt-get install -y gawk git-secret
