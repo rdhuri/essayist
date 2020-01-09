7ccd97cc85034b673eef54636aa29e1f8739217a
Rahul Dhuri <rdhuri@equalexperts.com>
2020-01-09 14:45:45 +0530

    update reveal_secret.sh

--- resources/reveal_secret.sh
+++ resources/reveal_secret.sh
@@ -3,10 +3,10 @@
 PRIVATE_KEY=$1
 GIT_SECRET_PASSPHRASE=$2
 
+sudo apt-get install apt-transport-https
 echo "deb https://dl.bintray.com/sobolevn/deb git-secret main" | sudo tee -a /etc/apt/sources.list
 sudo wget -qO - https://api.bintray.com/users/sobolevn/keys/gpg/public.key | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install apt-transport-https
 sudo apt-get install -y git-secret
 sudo echo `which gpg`
 echo ${PRIVATE_KEY} | base64 --decode > private_key
