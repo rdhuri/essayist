d754fd4fad67f9304d5e191e93751892accddd5e
Rahul Dhuri <rdhuri@equalexperts.com>
2020-01-09 14:57:29 +0530

    update reveal_secret.sh

--- resources/reveal_secret.sh
+++ resources/reveal_secret.sh
@@ -9,6 +9,7 @@ sudo wget -qO - https://api.bintray.com/users/sobolevn/keys/gpg/public.key | sud
 sudo apt-get update
 sudo apt-get install -y git-secret
 sudo echo `which gpg`
+echo "private key ${PRIVATE_KEY}"
 echo $PRIVATE_KEY | base64 --decode > private_key
 gpg --import --no-tty --batch --yes private_key
 
