ee3ea9d6869246829b2634557a79605a7a64e88d
Rahul Dhuri <rdhuri@equalexperts.com>
2020-01-09 14:05:08 +0530

    update reveal_secret.sh

--- resources/reveal_secret.sh
+++ resources/reveal_secret.sh
@@ -3,8 +3,8 @@
 PRIVATE_KEY=$1
 GIT_SECRET_PASSPHRASE=$2
 
-sudo sh -c "echo 'deb https://dl.bintray.com/sobolevn/deb git-secret main' | tee -a /etc/apt/sources.list"
-sudo wget -qO - https://api.bintray.com/users/sobolevn/keys/gpg/public.key | sudo apt-key add -
+#sudo sh -c "echo 'deb https://dl.bintray.com/sobolevn/deb git-secret main' | tee -a /etc/apt/sources.list"
+#sudo wget -qO - https://api.bintray.com/users/sobolevn/keys/gpg/public.key | sudo apt-key add -
 sudo apt-get update && sudo apt-get install -y gawk git-secret
 sudo echo `which gpg`
 echo ${PRIVATE_KEY} | base64 --decode > private_key
