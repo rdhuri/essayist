7770d2c28d00fef97ba6652b06dcea19ebf857e6
Rahul Dhuri <44663996+rdhuri@users.noreply.github.com>
2020-01-09 15:48:45 +0530

    Clean up (#3)
    * Add GitSecret
    
    * [FIX] Git secret reveal issue
    
    * Update config.yml
    
    * Update config.yml
    
    * Update config.yml
    
    * Update config.yml
    
    * Update config.yml
    
    * Add git secret reveal script
    
    * update config.yml
    
    * update reveal_secret.sh
    
    * Set permission to revel-secret.sh
    
    * update reveal_secret.sh
    
    * update reveal_secret.sh
    
    * update reveal_secret.sh
    
    * update reveal_secret.sh
    
    * update reveal_secret.sh
    
    * update reveal_secret.sh
    
    * update reveal_secret.sh
    
    * update reveal_secret.sh
    
    * update reveal_secret.sh
    
    * update reveal_secret.sh
    
    * update reveal_secret.sh
    
    * update reveal_secret.sh
    
    * clean up
    
    Co-authored-by: Rahul Dhuri <45159702+rdhuri7@users.noreply.github.com>

--- resources/reveal_secret.sh
+++ resources/reveal_secret.sh
@@ -9,9 +9,6 @@ sudo wget -qO - https://api.bintray.com/users/sobolevn/keys/gpg/public.key | sud
 sudo apt-get update
 sudo apt-get install -y git-secret
 sudo echo `which gpg`
-echo "private key ${PRIVATE_KEY}"
-#echo -e ${PRIVATE_KEY} > private_key.asc
 echo ${PRIVATE_KEY} | base64 --decode > private_key.asc
 gpg --import --no-tty --batch --yes private_key.asc
-echo "Git secret passphrase ${GIT_SECRET_PASSPHRASE}"
 git secret reveal -p ${GIT_SECRET_PASSPHRASE}
